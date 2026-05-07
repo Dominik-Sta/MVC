ae91effb8b359009505d919e154d0bda318ff606
Dominik Stawicki <stawido1144_aehit@students.vizja.pl>
2026-05-07 21:48:46 +0200

    Lab3cz5(zaraz znowu wszystko mi się usunie :(

--- DotNet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ DotNet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a006830c653f3085f945ed55f2c554c468c3e8e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7745322ab1792fca7cb893c49551d2a94cc8d39f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
